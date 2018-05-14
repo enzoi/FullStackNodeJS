0c621ffdd30ae0a274180dab0355b902e6b9561e
Yeontae Kim <ytk007@gmail.com>
2018-05-13 23:33:07 -0700

    Handle prod env keys

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID: '523813859627-q5olmd5bq8lbqhjuj2ta0cichrmes9vh.apps.googleusercontent.com',
+  googleClientSecret: 'vnG3KuA7c1YZ0bzx9o2U1kFp',
+  mongoURI: 'mongodb://yeontae:Kyt98515@ds119820.mlab.com:19820/emaily-dev-node',
+  cookieKey: 'adlskjfhasdkjhfkahsdlfjahlsdhf'
+};
