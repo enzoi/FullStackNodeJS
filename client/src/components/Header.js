16dcea6c4625cbd5775e4f65abdc2b6f9c5a8931
Yeontae Kim <ytk007@gmail.com>
2019-01-13 23:39:24 -0800

    Finish Header design

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,9 +3,18 @@ import React from 'react';
 class Header extends React.Component {
     render() {
         return (
-            <div>
-                Header
-            </div>
+            <nav>
+                <div className="nav-wrapper">
+                    <a className="left brand-logo">
+                        Emaily
+                    </a>
+                    <ul className="right">
+                        <li>
+                            <a>Login With Google</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
         );
     }
 }
