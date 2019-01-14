050b6ca2ddb445a2dd393b74dfe0eb87ff0018ca
Yeontae Kim <ytk007@gmail.com>
2019-01-14 11:34:35 -0800

    Add Payments component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import Payments from './Payments';
 
 class Header extends React.Component {
     renderAdmin() {
@@ -12,7 +13,10 @@ class Header extends React.Component {
                     <li><a href="/auth/google">Login With Google</a></li>  
                 );
             default:
-                return <li><a href="/api/logout">Logout</a></li>;
+                return [
+                    <li><Payments /></li>,
+                    <li><a href="/api/logout">Logout</a></li>
+                ];
        }
     }
 
