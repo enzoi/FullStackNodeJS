9de478e3d197a6def6d3901f068c942c63085651
Yeontae Kim <ytk007@gmail.com>
2019-01-23 15:30:50 -0800

    Update email template with redirect route

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -7,7 +7,11 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-    app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+    app.get('/api/surveys/thanks', (req, res) => {
+        res.send('Thanks for voting!');
+    });
+
+    app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
         const { title, subject, body, recipients } = req.body;
         
         const survey = new Survey({
@@ -18,9 +22,19 @@ module.exports = app => {
             _user: req.user.id,
             dateSent: Date.now()
          });
-         
+
          // Send an email
          const mailer = new Mailer(survey, surveyTemplate(survey));
-         mailer.send();
+
+         try {
+            await mailer.send();
+            await survey.save();
+            req.user.credits -= 1;
+            const user = await req.user.save();
+   
+            res.send(user);
+         } catch (err) {
+            res.status(422).send(err);
+         }
     });
 };
